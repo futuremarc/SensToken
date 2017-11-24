cf0a877bbe8574a3962f84023cc64dc4fc5b14f9
futuremarc <mas1529@nyu.edu>
2017-11-24 09:57:41 -0800

    deploys to rinkeby

--- src/config.js
+++ src/config.js
@@ -1,5 +1,9 @@
 let rpcUrl;
 process.env.NODE_ENV !== 'production' ? rpcUrl = 'http://localhost:9545' : rpcUrl = 'https://rinkeby.infura.io/';
-const config = {rpcUrl};
+const config = {
+  rpcUrl,
+  localMnemonic: 'senior field myself usage spare purse lecture worry harsh mercy interest exile',
+  rinkebyMnemonic: 'slender lyrics cloud rifle author enhance tray jelly vapor item blame coin'
+};
 
 export default config;
--- truffle.js
+++ truffle.js
@@ -1,4 +1,7 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
+  ontracts_build_directory: "./output",
   networks: {
     development: {
       host: "localhost",
@@ -6,8 +9,9 @@ module.exports = {
       network_id: "*"
     },
     rinkeby: {
-      host: "https://rinkeby.infura.io/",
-      port: 80,
+      host: "localhost",
+      port: 9546,
+      network_id: 4,
       from: "0x821aEa9a577a9b44299B9c15c88cf3087F3b5544",
       gas: 6721975
     },
@@ -16,6 +20,18 @@ module.exports = {
   			enabled: true,
   			runs: 200
   		}
-  	}
+  	},
+  rinkeby: {
+    provider: function() {
+      return new HDWalletProvider("slender lyrics cloud rifle author enhance tray jelly vapor item blame coin", "https://rinkeby.infura.io/");
+    },
+    network_id: '4'
+  },
+  test: {
+    provider: function() {
+      return new HDWalletProvider("senior field myself usage spare purse lecture worry harsh mercy interest exile", "http://localhost:8546/");
+    },
+    network_id: '*'
+  },
   }
 };
